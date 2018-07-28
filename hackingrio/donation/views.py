2aa008e8fb0efa1d9f4e3beec87e7277343410a4
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-07-28 15:04:12 -0300

    base.html

--- hackingrio/donation/views.py
+++ hackingrio/donation/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
+from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
+
+
+
+def signup(request):
+    return render(request, 'donation/signup.html', )
