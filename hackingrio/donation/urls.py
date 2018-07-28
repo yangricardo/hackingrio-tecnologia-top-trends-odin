609a3009f4389af1d919ffddf64015461fac77a8
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-07-28 18:15:06 -0300

    requirements

--- hackingrio/donation/urls.py
+++ hackingrio/donation/urls.py
@@ -7,6 +7,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('donate/', views.donation, name='donate'),
     path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
--- hackingrio/donation/views.py
+++ hackingrio/donation/views.py
@@ -1,17 +1,29 @@
 from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
-
+from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
+from .forms import DonationForm
+from .models import Beneficiary, Donation, DonatedBeneficiary
 
 def home(request):
 
     return render(request, 'home.html', )
 
 def donation(request):
+    user = User.objects.first() 
+    if request.method == 'POST':
+        form = DonationForm(request.POST)
+        if form.is_valid():
+            donation = form.save(commit=False)
+            donation.donated_by = user
+            donation.save()
 
-    return render(request, 'donation.html', )
+            # return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+    else:
+        form = DonationForm()
+    return render(request, 'donation.html',  {'form': form})
 
 def signup(request):
     if request.method == 'POST':
