33a244a1ca663c910f942f5931368d0a810dd1bf
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2018-07-28 15:15:19 -0300

    config

--- hackingrio/hackingrio/settings.py
+++ hackingrio/hackingrio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['localhost','127.0.0.1','0.0.0.0']
 # Application definition
 
 INSTALLED_APPS = [
+    'donation.apps.DonationConfig',
     'widget_tweaks',
     'django.contrib.admin',
     'django.contrib.auth',
